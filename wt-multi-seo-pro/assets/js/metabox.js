f60ac779a92c34132e4b84a929a60e64e808134d
昨日江郎 <milad@vip.qq.com>
2026-05-15 13:17:46 +0800

    update the plugin

--- wt-multi-seo-pro/assets/js/metabox.js
+++
@@ -1,7 +0,0 @@
-jQuery(function ($) {
-    $('textarea[maxlength], input[maxlength]').on('input', function () {
-        var max = parseInt($(this).attr('maxlength'), 10);
-        var value = $(this).val() || '';
-        $(this).attr('data-count', value.length + '/' + max);
-    });
-});
